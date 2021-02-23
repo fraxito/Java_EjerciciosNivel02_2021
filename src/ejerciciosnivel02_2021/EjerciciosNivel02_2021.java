3e21122c291686ad6e8aec0cd4cc5d337dc80ca3
fraxito <fraxito@gmail.com>
2021-02-23 11:48:37 +0100

    Con el strstr empezado. Hay que probarlo porque no funciona todavía

--- src/ejerciciosnivel02_2021/EjerciciosNivel02_2021.java
+++ src/ejerciciosnivel02_2021/EjerciciosNivel02_2021.java
@@ -54,6 +54,64 @@ public class EjerciciosNivel02_2021 {
     }
     
     /**
+     * Encuentra el número que está duplicado en una lista de ints
+     * usando el algoritmo de Floyd
+     * @param listaNumeros
+     * @return 
+         0  1  2  3  4    */
+    //  [1, 3, 2, 4, 3]
+    public int findDuplicate(int [] listaNumeros){
+        int tortuga = listaNumeros[0];
+        int liebre = listaNumeros[0];
+        boolean iguales = false;
+        while (!iguales){
+            tortuga = listaNumeros[tortuga];
+            liebre = listaNumeros[listaNumeros[liebre]];
+            if (tortuga == liebre){
+                iguales = true;
+            }
+        }
+        int aux1 = listaNumeros[0];
+        int aux2 = tortuga;
+        while (aux1 != aux2){
+            aux1 = listaNumeros[aux1];
+            aux2 = listaNumeros[aux2];
+        }
+        return aux1;
+    }
+    
+    public int strStr(String str1, String str2){
+        int posicion = -1;
+        //suponemos que no hay acentos y que todo es minúsculas
+        for (int i=0; i< str1.length(); i++){
+            
+            if (str1.charAt(i) == str2.charAt(0)){
+                posicion = i;
+                int j=0; //indice para buscar en el str2
+                while ( (j < str2.length() && i<str1.length()) && str1.charAt(i) == str2.charAt(j)){
+                    j++;
+                    i++;
+                }
+                //el while ha terminado y puede haber salido por varias razones
+                //la primera, que alguno de los dos lenght se haya terminado
+                //la segunda, que alguna letra sea distinta
+                
+                if (str1.charAt(i) == str2.charAt(j)){
+                    
+                }
+            }
+        }
+        
+        return posicion;
+    }
+    
+    
+    
+    
+    
+    
+    
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
@@ -68,6 +126,12 @@ public class EjerciciosNivel02_2021 {
            {'M', 'A', 'T', 'O'}
         };
         System.out.println(ejercicio.esEscaleraDePalabras(listaPalabras));
+        
+        System.out.println(ejercicio.findDuplicate(new int[]{3,2,3,4,1}));
+        System.out.println(ejercicio.findDuplicate(new int[]{4,2,3,4,1}));
+        System.out.println(ejercicio.findDuplicate(new int[]{1,1,3,4,1}));
+        
+        System.out.println(ejercicio.strStr("hola Helios!", "el"));
     }
     
 }
